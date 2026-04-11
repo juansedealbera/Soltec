c1a0b94ed051f1ac83085625e1104f3e6d193432
juansedealbera <dealberaj@gmail.com>
2026-04-10 23:41:21 -0300

    Soltec

---
+++ Soltec-CSS/layout/js/productos.js
@@ -0,0 +1,64 @@
+const productos = [
+    {
+        id: 1,
+        imagen: { 
+            src: './layout/imagenes/foto de auriculares.jpg', 
+            alt: 'Auriculares JBL' 
+        },
+        nombre: 'Auriculares JBL 520BT',
+        descripcion: 'Audio inalámbrico con sonido puro y batería de larga duración.',
+        precio: 59999
+    },
+    {
+        id: 2,
+        imagen: { 
+            src: './layout/imagenes/foto de parlante jbl.jpg', 
+            alt: 'Parlante JBL GO3' 
+        },
+        nombre: 'Parlante JBL GO3',
+        descripcion: 'Compacto, resistente al agua y con gran potencia.',
+        precio: 42500
+    },
+    {
+        id: 3,
+        imagen: { 
+            src: './layout/imagenes/foto de auriculares tuneXbuds.jpg', 
+            alt: 'Tune Xbuds' 
+        },
+        nombre: 'Auriculares Tune Xbuds',
+        descripcion: 'Diseño ergonómico y excelente aislamiento de ruido.',
+        precio: 38900
+    },
+    {
+        id: 4,
+        imagen: { 
+            src: './layout/imagenes/Foto de auriculares bt.jpg', 
+            alt: 'Auriculares Bluetooth' 
+        },
+        nombre: 'Auriculares Bluetooth JBL',
+        descripcion: 'La mejor calidad en el diseño y sonido.',
+        precio: 15000
+    },
+    {
+        id: 5,
+        imagen: { 
+            src: './layout/imagenes/mouse rgb.jpg',
+            alt: 'Mouse Gamer RGB'
+        },
+        nombre: 'Mouse Gamer RGB',
+        descripcion: 'Entrada de gama, 256 gramos e iluminación RGB.',
+        precio: 9500
+    },
+    {
+        id: 6,
+        imagen: { 
+            src: './layout/imagenes/auricular.jpeg' ,
+            alt:'Mouse Inalámbrico'
+        },
+        nombre: 'Mouse Inalámbrico',
+        descripcion: 'Mouse inalámbrico de oficina, económico, práctico y duradero.',
+        precio: 7800
+    },
+];
+
+export default productos
